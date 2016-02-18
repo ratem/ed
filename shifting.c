d73175996cceae02e0506cf6a2e4494596ce7e94
Rogerio Atem Carvalho <ratem@iff.edu.br>
2016-02-18 16:51:58 -0200

    Bitwise operators e shifting

---
+++ shifting.c
@@ -0,0 +1,50 @@
+/*
+	Este programa simula a preparacao para transmissao via rede de um frame composto por um campo de um byte 
+	e outro de dois bytes, onde o primeiro representa um booleano (apenas um bit) e o segundo endereca ateh
+	1024 (usando 10 bits). O frame eh entao compactado para caber em apenas 11 bits, cuja representacao minima 
+	eh uma variavel de 16bits.
+*/
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct
+{
+	uint8_t type;
+	uint16_t address;	
+} frame_t;
+
+typedef struct
+{
+	uint16_t data;
+} packet_t;
+
+packet_t pack(frame_t frame)
+{
+	packet_t packet;
+	uint16_t buffer;
+
+	packet.data = frame.address;		//Copia os 10 primeiros bits
+	buffer = frame.type;				//Guarda o boolean num buffer de 16 bits
+	buffer = buffer << 15;				//Desloca o bit para a extrema esquerda
+	packet.data = packet.data | buffer;	//Funde os dois valores
+}
+
+frame_t unpack(packet_t packet)
+{
+}
+
+void print16bits(uint16_t data)
+{
+	int8_t i;
+
+	for(i = 15; i >= 0; i--)
+		printf("%u ", data&(1 << i) ? 1 : 0);//Seleciona bit a bit e imprime apenas esse bit usando o ? 
+	printf("\n");
+}
+
+int main(void)
+{
+	print16bits(36555);
+	return 0;
+}
+
